6142ad23fa7b33f052803d8be87f2507167134af
Rashmi <rashmimaniampare@gmal.com>
2026-01-21 13:07:18 +0530

    clear assets app and add new assests

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+  return (
+    <main>app</main>
+  )
+}
+
+export default App
